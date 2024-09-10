d2da29bd0ef02c4a4cb112c59b2e2e8e4c82cf86
berrazuriz1 <berrazuriz1@uc.cl>
2024-09-10 10:36:09 -0700

    fix configuration to function ruby

--- llama-index-packs/llama-index-packs-code-hierarchy/llama_index/packs/code_hierarchy/code_hierarchy.py
+++ llama-index-packs/llama-index-packs-code-hierarchy/llama_index/packs/code_hierarchy/code_hierarchy.py
@@ -165,10 +165,11 @@ _DEFAULT_SIGNATURE_IDENTIFIERS: Dict[str, Dict[str, _SignatureCaptureOptions]] =
             name_identifier="constant",
         ),
         "method": _SignatureCaptureOptions(
-            end_signature_types=[
+            start_signature_types=[
                 _SignatureCaptureType(type="body_statement", inclusive=False),
                 _SignatureCaptureType(type="comment", inclusive=False),
             ],
+            end_signature_types=[_SignatureCaptureType(type="end", inclusive=True)],
             name_identifier="identifier",
         ),
     },
